0e701c9ad0f160543f3f428ba1e25b7f70abc31f
Rob Hess <hessro@oregonstate.edu>
2024-03-13 15:40:48 -0700

    Add code from 3/13 lecture.

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: ".env.local" })
+
+const client_id = process.env.VITE_GITHUB_CLIENT_ID
+const client_secret = process.env.GITHUB_CLIENT_SECRET
+let access_token = null
+
+console.log("== client_id:", client_id)
+console.log("== client_secret:", client_secret)
 
 const USER = {
   username: "luke",
@@ -26,8 +36,34 @@ app.get("/api/user", (req, res) => {
   res.status(200).send(body)
 })
 
-app.get("/api/tokenExchange", (req, res) => {
-  res.status(200).send({ msg: "OK!" })
+app.post("/api/tokenExchange", async (req, res) => {
+  const { code } = req.body
+  console.log("== code:", code)
+  if (!code) {
+    res.status(400).send({ err: "Must specify auth code" })
+  } else {
+    const githubRes = await fetch("https://github.com/login/oauth/access_token", {
+      method: "POST",
+      body: JSON.stringify({
+        client_id: client_id,
+        client_secret: client_secret,
+        code: code
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+    const githubResBody = await githubRes.json()
+    if (githubResBody.access_token) {
+      access_token = githubResBody.access_token
+      res.status(200).send({ msg: "OK!" })
+    } else {
+      res.status(401).send({
+        err: githubResBody.error_description
+      })
+    }
+  }
 })
 
 app.listen(port, () => console.log(`API server listening on port ${port}`))
--- src/pages/GitHubLogin.jsx
+++ src/pages/GitHubLogin.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 import GitHubLoginLink from '../components/GitHubLoginLink'
 
 export default function GitHubLogin() {
   const [ error, setError ] = useState("")
+  const [ searchParams, setSearchParams ] = useSearchParams()
+  const [ success, setSuccess ] = useState(false)
+  const authCode = searchParams.get("code")
+
+  console.log("== authCode:", authCode)
+  useEffect(() => {
+    async function exchangeForAccessToken(code) {
+      const res = await fetch("/api/tokenExchange", {
+        method: "POST",
+        body: JSON.stringify({ code }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      if (res.status !== 200) {
+        setError("Error exchanging code for token")
+      } else {
+        setSuccess(true)
+      }
+    }
+    if (authCode) {
+      exchangeForAccessToken(authCode)
+    }
+  }, [ authCode ])
 
   return (
     <div>
       {error && <p>Error: {error}</p>}
-      <GitHubLoginLink />
+      {success ? <p>Success!</p> : <GitHubLoginLink />}
     </div>
   )
 }
